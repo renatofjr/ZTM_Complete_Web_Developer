42731f66018c4387c0310a9070497c920a576536
renatofjr <renatofjr@gmail.com>
2022-06-30 21:00:42 -0300

    'nodejs'

--- nodejs/script.js
+++ nodejs/script.js
@@ -1,9 +1,7 @@
-// const c = require("./script2");
-import { largeNumber } from "./script2";
+const c = require("./script2");
 
-// const a = c.largeNumber;
-const a = largeNumber;
-const b = 5;
+const a = c.largeNumber;
+const b = 7;
 
 console.log(a+b)
 
--- nodejs/script2.js
+++ nodejs/script2.js
@@ -1,5 +1,5 @@
-export const largeNumber = 356;
+const largeNumber = 356;
 
-// module.exports = {
-//     largeNumber: largeNumber
-// } 
+module.exports = {
+    largeNumber: largeNumber
+} 
