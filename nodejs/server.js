1fd95c0fe40c43c6b9f071e0131a8e47070c0130
renatofjr <renatofjr@gmail.com>
2022-07-06 21:18:59 -0300

    'release'

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,21 +1,8 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => {
-    res.send('getting root')
-})
 
-app.get('/profile', (req, res) => {
-    res.send('getting profile')
-})
-
-app.post('/profile', (req, res) => {
-    console.log(req.body)
-    const user = {
-        name: 'Sally',
-        hobby: 'soccer'
-    }
-    res.send(user)
-})
 app.listen(3000)
